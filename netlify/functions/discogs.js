505b0be595bc76b58232baffc4bc74259eddc441
Eugene Molari <eugelogic@gmail.com>
2021-08-10 16:39:42 +0100

    Add Search By Year feature

--- netlify/functions/discogs.js
+++ netlify/functions/discogs.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch')
 
-exports.handler = async () => {
-    const DISCOGS_API = `https://api.discogs.com/database/search?genre=Jazz&year=2021&token=${process.env.DISCOGS_TOKEN}`
+exports.handler = async (event) => {
+    const eventQueryStringPar = event.queryStringParameters
+    const DISCOGS_API = `https://api.discogs.com/database/search?genre=Jazz&year=${eventQueryStringPar.year}&token=${process.env.DISCOGS_TOKEN}`
 
     const response = await fetch(DISCOGS_API)
     const data = await response.json()
--- src/JazzRecords.js
+++ src/JazzRecords.js
@@ -4,7 +4,9 @@ import axios from "axios"
 
 function JazzRecords() {
   const [data, setData] = useState({ results: [] })
-  const [url] = useState("/.netlify/functions/discogs")
+  const currentYear = new Date().getFullYear()
+  const [query, setQuery] = useState(currentYear)
+  const [url, setUrl] = useState(`/.netlify/functions/discogs?genre=Jazz&year=${query}`)
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
 
@@ -23,9 +25,27 @@ function JazzRecords() {
     fetchData()
   }, [url])
 
+  function handleSubmit(el) {
+    el.preventDefault()
+    if (query.length === 0) return
+    setUrl(`/.netlify/functions/discogs?genre=Jazz&year=${query}`)
+  }
+
+  function handleChange(el) {
+    setQuery(el.target.value)
+  }
+
   return (
     <div>
-      <h1>Jazz Records Published in 2021</h1>
+      <h1>Search Jazz Records Published by Year</h1>
+      <form onSubmit={handleSubmit} >
+        <input
+          type="text"
+          value={query}
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
       <br />
       {isError && <p>Something went wrong :-/</p>}
       {isLoading ? (
