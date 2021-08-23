6eb59c4b6f251c4c20d7a665f481c1793db4cd6a
Eugene Molari <eugelogic@gmail.com>
2021-08-23 16:25:50 +0100

    Add 'Most Collected' filter to query string

--- netlify/functions/discogs.js
+++ netlify/functions/discogs.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 
 exports.handler = async (event) => {
     const eventQueryStringPar = event.queryStringParameters
-    const DISCOGS_API = `https://api.discogs.com/database/search?genre=Jazz&year=${eventQueryStringPar.year}&token=${process.env.DISCOGS_TOKEN}`
+    const DISCOGS_API = `https://api.discogs.com/database/search?genre=Jazz&year=${eventQueryStringPar.year}&sort=have&token=${process.env.DISCOGS_TOKEN}`
 
     const response = await fetch(DISCOGS_API)
     const data = await response.json()
